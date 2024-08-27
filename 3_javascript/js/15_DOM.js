0ce932d46a55e424e6f80a0f7fef9e18a01e8776
louis9494 <louis9494@naver.com>
2024-08-27 16:21:29 +0900

    240827수정

--- 3_javascript/js/15_DOM.js
+++ 3_javascript/js/15_DOM.js
@@ -1,7 +1,7 @@
 // Node 확인하기
 document.querySelector("#btn1").addEventListener("click", () => {
 
-  const test = document.querySelector("#test"); //ul
+  const test = document.querySelector("#test"); // ul
 
   console.log(test);
 
@@ -9,13 +9,13 @@ document.querySelector("#btn1").addEventListener("click", () => {
   const list = test.childNodes;
   console.log(list); // NodeList -> 유사배열
 
-  // 자식 노드 중 첫번째 li 요소
+  // 자식 노드 중 첫번째 li 요소 
   const li1 = list[3];
   console.log(li1);
 
   // 1. 부모 노드 찾기 (요소.parentNode)
   // -> li1 부모 노드 배경색 변경
-  li1.parentNode.style.backgroundColor = "skyblue"
+  li1.parentNode.style.backgroundColor = "skyblue";
 
   // 2. 첫 번째 자식 노드 찾기 (요소.firstChild)
   // -> #test(ul)의 첫 번째 자식 노드 찾기
@@ -28,7 +28,7 @@ document.querySelector("#btn1").addEventListener("click", () => {
   // 4. 원하는 위치(index)에 존재하는 자식 노드 찾기
   // (요소.childNodes[index])
   // -> #test의 자식 노드 중
-  // 3번째 li 태그를 찾아서 배경색 piink 지정하기
+  // 3번째 li 태그를 찾아서 배경색 pink 지정하기
   test.childNodes[9].style.backgroundColor = "pink";
 
   // 5. 이전/다음 형제 노드 찾기
@@ -37,36 +37,37 @@ document.querySelector("#btn1").addEventListener("click", () => {
 
   // -> 탐색용 코드는 연달아서 작성 가능!
 
-  // test.childNode[9]의 다음 다음 형제 노드 선택
-  // test.childNoded[11]
+  // test.childNodes[9]의 다음 다음 형제 노드 선택
+  // == test.childNodes[11]
   console.log(test.childNodes[9].nextSibling.nextSibling);
 
-  //#test의 마지막 자식 노드의 이전 형제 노드 선택
+  // #test의 마지막 자식 노드의 이전 형제 노드 선택
   console.log(test.lastChild.previousSibling);
 
-  //노드 추가
 
-  //1. 마지막 자식 노드로 추가하기 ( appendChild(노드))
+  // 노드 추가
+
+  // 1. 마지막 자식 노드로 추가 ( appendChild(노드) )
 
   // -> list[11]의 마지막 자식으로 "ZZZ" 라는 TextNode 추가
-  list[11].appendChild(document.createTextNode("ZZZ") );
+  list[11].appendChild( document.createTextNode("ZZZ") );
 
-  // 2. 마지막 자식으로 추가 ( append("내용" 또는 노드 또는 요소 * n))
-  list[11].append("12345", "abc", "가나다라");
+  // 2. 마지막 자식으로 추가 ( append("내용" 또는 노드 또는 요소 * n) )
+  list[11].append("12345", "abcd", "가나다라");
 
-  // 3. 첫 번쨰 자식으로 추가 (prepend("내용" 또는 노드 또는 요소 * n))
+  // 3. 첫 번째 자식으로 추가 ( prepend("내용" 또는 노드 또는 요소 * n) )
   list[11].prepend("오늘", "점심", "뭐먹지?");
 
-  //4. 이전/다음 형제로 추가 (before/after("내용" 또는 노드 또는 요소))
+  // 4. 이전/다음 형제로 추가 ( before/after("내용" 또는 노드 또는 요소) )
   // #test 전/후에 내용 추가
   test.before("이전 입니다");
   test.after("이후 입니다");
 });
 
-// ---------------------------------------------------
+// --------------------------------------
 
-
-/*children : 자식 요소만 모두 선택
+/*
+  children : 자식 요소만 모두 선택
   parentElement : 부모 요소 선택
 
   firstElementChild : 첫 번째 자식 요소 선택
@@ -74,23 +75,27 @@ document.querySelector("#btn1").addEventListener("click", () => {
 
   previousElementSibling : 이전 형제 요소 선택
   nextElementSibling     : 다음 형제 요소 선택
- * 
- * 
- * 
- */
+
+*/
+
 const btn2 = document.querySelector("#btn2");
 
 btn2.addEventListener("click", () => {
-  //#test2 얻어오기(ul)
+
+  // #test2 얻어오기 (ul)
   const test2 = document.querySelector("#test2");
-  //#test의 모든 자식 요소 (children)
+
+  // #test2의 모든 자식 요소(children)
   console.log(test2.children);
 
-  //#test의 첫 번째 자식 요소 찾기
+  // #test2의 첫 번째 자식 요소 찾기
   console.log(test2.firstElementChild);
 
-  //#test2의 마지막 자식 요소 찾기
+  // #test2의 마지막 자식 요소 찾기
   console.log(test2.lastElementChild);
+
+  // #test2 부모 요소 
+  console.log(test2.parentElement);
 
   // #test2 이전 형제 요소
   console.log(test2.previousElementSibling);
